65147bbb92a1c236aa070fd93a102cfc8846ef4a
an-jonas <anjonas@student.ethz.ch>
2021-10-28 11:32:45 +0200

    Image selection tools added

---
+++ run_select_images.py
@@ -0,0 +1,22 @@
+
+# ======================================================================================================================
+# Author: Jonas Anderegg, jonas.anderegg@usys.ethz.ch
+# Project: MixedInf
+# Date: 27.10.2021
+# Sample images amenable to further analysis
+# ======================================================================================================================
+
+from image_selector import ImageSelector
+
+
+def run():
+    dir_images = "P:/Public/Jonas/004_Divers/Test2/Evaluation/orig"
+    dir_overlays = "P:/Public/Jonas/004_Divers/Test2/Evaluation/processed/overlay"
+    image_selector = ImageSelector(dir_images, dir_overlays)
+    image_selector.select_images()
+
+
+if __name__ == '__main__':
+    run()
+
+
